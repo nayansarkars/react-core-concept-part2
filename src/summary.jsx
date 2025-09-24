7af22809aed28acf4f64ef3c05a5e319ccf53a68
Sarkar Nayan <web.sarkarnayan@gmail.com>
2025-09-24 09:10:34 +0600

    State

--- src/summary.jsx
+++ src/summary.jsx
@@ -29,3 +29,11 @@
 //     const data = res.json();
 //     return data;
 // }
+
+/**
+ * 1. Event tigger
+ * 2. State
+ * 3. Data load from API
+ * 4. Loop through to display data
+ * 5. why we use key prop
+ */
